75890da7c51e091f05f0851511267b43b6dd2170
Karmanya Tyagi <karmanyatyagi@Karmanyas-MacBook-Air.local>
2024-10-22 23:31:27 +0530

    second

---
+++ 02_Variables.py
@@ -0,0 +1,40 @@
+# Python Variables and Data Types
+
+# String
+
+name = "Karmanya"
+
+# Integer
+age = 24
+
+
+# Float
+height = 5.9
+
+# Boolean
+is_student = True
+
+
+# List (Array equivalent in Python)
+skills = ["Python","Javascript","Cybersecurity"]
+
+
+
+# Tuple (Immutable list)
+
+coordinates = (10.0,20.0)
+
+
+# Dictionary (Key-Value pair)
+
+profile = {"name":"Karmanya","age":24,"professional":"Information Security Analyst"}
+
+
+# Outputting Variables
+print(f"Name:{name},Age:{age},Height:{height},Is Student: {is_student}")
+print(f"Skills:{skills}")
+print(f"Coordinates:{coordinates}")
+print(f"Profile: {profile}")
+
+
+
