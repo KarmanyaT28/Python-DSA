aca88586de7d6e58d994e101c188e9ee5562c849
Karmanya Tyagi <karmanyatyagi@Karmanyas-MacBook-Air.local>
2024-10-29 23:46:50 +0530

    lists

---
+++ 07_Lists.py
@@ -0,0 +1,24 @@
+# Creating a list
+
+fruits = ["apple","banana","cherry"]
+
+
+# Accessing elements
+print(fruits[0])
+
+
+# Adding elements
+fruits.append("orange")
+print(fruits)
+
+
+# Removing elements
+fruits.remove("banana")
+print(fruits)
+
+# Length of list
+print(len(fruits))
+
+
+# Slicing a list
+print(fruits[1:3})
