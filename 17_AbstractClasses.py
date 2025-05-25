3bb0d4e5361f990e294040f722658da9a9479d53
KarmanyaT <61420434+KarmanyaT28@users.noreply.github.com>
2025-05-26 03:37:23 +0530

    Update 17_AbstractClasses.py

--- 17_AbstractClasses.py
+++ 17_AbstractClasses.py
@@ -1,1 +1,1 @@
-<button onclick="window.location.href='{% url 'form_dropdown_page' %}'">Pending Approval</button>
+<button onclick="window.location.href='{% url "form_dropdown_page" %}'">Pending Approval</button>
